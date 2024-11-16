b67922bcbfe7ea8ef5934e0a6147e7603c1ec073
aalhitennf <paananen.olli@tuta.io>
2024-11-16 16:25:16 +0200

    Scan style folder recursively

--- parser/src/read.rs
+++ parser/src/read.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 /// Default read buffer size
 const DEFAULT_BUF_SIZE: usize = 16 * 1024;
 
@@ -8,29 +10,39 @@ const DEFAULT_BUF_SIZE: usize = 16 * 1024;
 /// # Errors
 /// Returns `std::io::Error` if path is not readable
 pub fn read_styles(path: &std::path::Path) -> Result<String, std::io::Error> {
-    if path.is_dir() {
-        let combined = std::fs::read_dir(path)?
-            .filter_map(Result::ok)
-            .filter_map(|e| {
-                e.path()
-                    .extension()
-                    .is_some_and(|e| e.eq_ignore_ascii_case("css"))
-                    .then_some(e.path())
-            })
-            .filter_map(|path| match std::fs::read_to_string(&path) {
-                Err(e) => {
-                    log::warn!("Failed to read file {path:?}\n{e}");
-                    None
-                }
-                Ok(v) => Some(v),
-            })
-            .fold(String::with_capacity(DEFAULT_BUF_SIZE), |mut s, c| {
-                s.push_str(&c);
-                s
-            });
-
-        Ok(combined)
-    } else {
+    if path.is_file() {
         Ok(std::fs::read_to_string(path)?)
+    } else {
+        let mut buf = String::with_capacity(DEFAULT_BUF_SIZE);
+        read_dir_recursive(&mut buf, path);
+        Ok(buf)
+    }
+}
+
+fn read_dir_recursive(buf: &mut String, path: &Path) {
+    let dir_entry = match std::fs::read_dir(path) {
+        Ok(dir_entry) => dir_entry,
+        Err(e) => {
+            log::warn!("Failed to read {path:?}: {e}");
+            return;
+        }
+    };
+    for entry in dir_entry.flatten() {
+        let entry_path = entry.path();
+        if entry_path
+            .extension()
+            .is_some_and(|ext| ext.eq_ignore_ascii_case("css"))
+        {
+            match std::fs::read_to_string(&entry_path) {
+                Ok(content) => {
+                    buf.push_str(&content);
+                }
+                Err(e) => {
+                    log::warn!("Failed to read {entry_path:?}: {e}");
+                }
+            }
+        } else if entry_path.is_dir() {
+            read_dir_recursive(buf, &entry_path);
+        }
     }
 }
